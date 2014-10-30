ab1ccac62cd3a79b4eb9a8d036081da7470b5504
Jeremy McRae <jm33@sanger.ac.uk>
2014-10-30 20:15:54 +0000

    update VUS variant script to exclude both parts of a compound het where one is non-lof

--- scripts/get_vus_variants.py
+++ scripts/get_vus_variants.py
@@ -11,7 +11,8 @@ including:
       variants. 
 
 This script also:
-    - annotates whether the gene is in the OMIM morbid map genes (see http://compbio.charite.de/hudson/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/genes_to_diseases.txt)
+    - annotates whether the gene is in the OMIM morbid map genes 
+        (see http://compbio.charite.de/hudson/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/genes_to_diseases.txt)
     - filters out variants in genes with multiple (>1) common (MAF >= 0.05) LOF
       variants (as determined from 1000 Genomes datasets).
 """
@@ -59,13 +60,16 @@ def open_variants(filename):
             continue
         
         # get the info for a unique key to identify the variant
-        split_line = line.strip().split("\t")
-        individual_id = split_line[0]
-        chrom = split_line[3]
-        position = split_line[4]
+        line = line.strip().split("\t")
+        individual_id = line[0]
+        chrom = line[3]
+        position = line[4]
         
         key = (individual_id, chrom, position)
-        variants[key] = line
+        if individual_id not in variants:
+            variants[individual_id] = {}
+        
+        variants[individual_id][key] = line
     
     return variants
 
@@ -127,30 +131,42 @@ def remove_common_lof_genes(variants, lof_genes):
     """
     
     without_common_lofs = {}
-    for key in variants:
-        line = variants[key]
-        split_line = line.strip().split("\t")
-        hgnc_symbols = split_line[5].split(",")
-        
-        # find which genes for a variant match the OMIM morbid map genes
-        lof_match = False
-        for hgnc in hgnc_symbols:
-            if hgnc in lof_genes:
-                lof_match = True
-        
-        if not lof_match:
-            without_common_lofs[key] = line
+    for person_id in variants:
+        for key in variants[person_id]:
+            line = variants[person_id][key]
+            hgnc_symbols = line[5].split(",")
+            
+            # print(hgnc_symbols)
+            
+            # find which genes for a variant match the common LOF genes
+            common_lof_match = False
+            for hgnc in hgnc_symbols:
+                if hgnc in lof_genes:
+                    common_lof_match = True
+                    break
+            
+            if not common_lof_match:
+                if person_id not in without_common_lofs:
+                    without_common_lofs[person_id] = {}
+                without_common_lofs[person_id][key] = line
     
     return without_common_lofs
 
-def remove_overlap(all_vars, ddg2p_vars):
+def remove_ddg_overlap(all_vars, ddg2p_vars):
     """ removes all genes variants that overlap with the DDG2P variants
     """
     
     unique = {}
-    for key in all_vars:
-        if key not in ddg2p_vars:
-            unique[key] = all_vars[key]
+    for person_id in all_vars:
+        if person_id not in ddg2p_vars:
+            unique[person_id] = all_vars[person_id]
+        else:
+            for key in all_vars[person_id]:
+                if key not in ddg2p_vars[person_id]:
+                    if person_id not in unique:
+                        unique[person_id] = {}
+                    
+                    unique[person_id][key] = all_vars[person_id][key]
     
     return unique
 
@@ -159,11 +175,50 @@ def get_functional_de_novos(variants, de_novo_keys):
     """
     
     de_novos = {}
-    for key in variants:
-        if key in de_novo_keys:
-            de_novos[key] = variants[key]
+    for person_id in variants:
+        for key in variants[person_id]:
+            if key in de_novo_keys:
+                if person_id not in de_novos:
+                    de_novos[person_id] = {}
+                de_novos[person_id][key] = variants[person_id][key]
         
     return de_novos
+
+def is_compound_het_lof(key, variants, lof_consequences):
+    """ checks that both variants in a compound het are loss-of-function
+    """
+    
+    line = variants[key]
+    consequence = line[8].split(",")[0]
+    inheritance = line[11]
+    result = line[15]
+    gene = line[5]
+    
+    sample_id = key[0]
+    chrom = key[1]
+    
+    lof_compound_hets = 0
+    for alt_key in variants:
+        alt_sample_id = alt_key[0]
+        alt_chrom = alt_key[1]
+        
+        line = variants[alt_key]
+        alt_gene = line[5]
+        
+        alt_consequence = line[8].split(",")[0]
+        alt_inheritance = line[11]
+        alt_result = line[15]
+        
+        # skip past variant for different individuals, or on different chroms
+        if alt_sample_id != sample_id or alt_chrom != chrom or alt_gene != gene:
+            continue
+        
+        if alt_consequence in lof_consequences and "compound_het" in alt_result:
+            lof_compound_hets += 1
+        
+    print(key, lof_compound_hets >= 2)
+    
+    return lof_compound_hets >= 2
 
 def get_lof_recessive_variants(variants):
     """ gets recessive variants with loss-of-function consequences
@@ -181,14 +236,31 @@ def get_lof_recessive_variants(variants):
     dominant_inheritances = set(["Monoallelic", "X-linked dominant"])
     
     lof_variants = {}
-    for key in variants:
-        line = variants[key]
-        split_line = line.strip().split("\t")
-        consequence = split_line[8].split(",")[0]
-        inheritance = split_line[11]
-        
-        if consequence in lof_consequences and inheritance not in dominant_inheritances:
-            lof_variants[key] = line
+    for person_id in variants:
+        for key in variants[person_id]:
+            line = variants[person_id][key]
+            consequence = line[8].split(",")[0]
+            inheritance = line[11]
+            result = line[15]
+            
+            chrom = key[1]
+            
+            
+            # if the variant is part of a compound het, both variants have to be
+            # lof, so drop compound het variants where one of the pair is non-lof
+            if "compound_het" in result and not is_compound_het_lof(key, variants[person_id], lof_consequences):
+                print(key)
+                if result == "compound_het":
+                    continue
+                line[15] = "single_variant"
+            
+            # print(key, consequence in lof_consequences, inheritance not in dominant_inheritances)
+            
+            if consequence in lof_consequences and inheritance not in dominant_inheritances:
+                if person_id not in lof_variants:
+                    lof_variants[person_id] = {}
+                
+                lof_variants[person_id][key] = line
     
     return lof_variants
 
@@ -197,25 +269,25 @@ def annotate_omim_gene(variants, omim_genes):
     """
     
     annotated_vars = {}
-    for key in variants:
-        line = variants[key]
-        split_line = line.strip().split("\t")
-        hgnc_symbols = split_line[5].split(",")
-        
-        # find which genes for a variant match the OMIM morbid map genes
-        omim_match = []
-        for hgnc in hgnc_symbols:
-            if hgnc in omim_genes:
-                omim_match.append(hgnc)
-        
-        # annotate the variant with the omim match (or lack)
-        if len(omim_match) == 0:
-            line = line.rstrip() + ",NA\n"
-        else:
-            omim_match = ",".join(omim_match)
-            line = line.rstrip() + "," + omim_match + "\n"
-        
-        annotated_vars[key] = line
+    for person_id in variants:
+        for key in variants[person_id]:
+            line = variants[person_id][key]
+            hgnc_symbols = line[5].split(",")
+            
+            # find which genes for a variant match the OMIM morbid map genes
+            omim_match = []
+            for hgnc in hgnc_symbols:
+                if hgnc in omim_genes:
+                    omim_match.append(hgnc)
+            
+            # annotate the variant with the omim match (or lack)
+            if len(omim_match) == 0:
+                omim_match = ["NA"]
+            else:
+                omim_match = [",".join(omim_match)]
+            
+            line = "\t".join(line + omim_match) + "\n"
+            annotated_vars[key] = line
     
     return annotated_vars  
 
@@ -244,22 +316,27 @@ def main():
     # open the datasets for the script
     vus_vars = open_variants(vus_file)
     ddg2p_vars = open_variants(ddg2p_file)
-    omim_genes = open_omim_genes(omim_file)
     common_lof_genes = open_common_lof(common_lof_file)
+    
     de_novos = open_validated_denovos(de_novo_file)
+    de_novo_variants = get_functional_de_novos(vus_vars, de_novos)
     
     # filter the VUS variants
-    de_novos_variants = get_functional_de_novos(vus_vars, de_novos)
-    vus_vars = remove_overlap(vus_vars, ddg2p_vars)
+    vus_vars = remove_ddg_overlap(vus_vars, ddg2p_vars)
     vus_vars = get_lof_recessive_variants(vus_vars)
     vus_vars = remove_common_lof_genes(vus_vars, common_lof_genes)
     
-    # include any validated de novos
-    vus_vars.update(de_novos_variants)
+    # put all the validated de novos back in
+    for person_id in de_novo_variants:
+        if person_id not in vus_vars:
+            vus_vars[person_id] = {}
+        vus_vars[person_id].update(de_novo_variants[person_id])
     
     # annotate the variants with whether their genes are OMIM morbid map genes, 
     # and write the variants to a file
+    omim_genes = open_omim_genes(omim_file)
     vus_vars = annotate_omim_gene(vus_vars, omim_genes)
+    # print(vus_vars.keys())
     write_output(output_file, vus_vars)
 
 
