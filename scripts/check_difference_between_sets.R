7be99c7829665ea6b4a66225e3f81731fd7d4c59
Jeremy McRae <jm33@sanger.ac.uk>
2015-01-22 09:27:08 +0000

    small adjustments to script to check difference between runs

--- scripts/check_difference_between_sets.R
+++ scripts/check_difference_between_sets.R
@@ -88,12 +88,12 @@ check_variant_fail <- function(variant, ped_path) {
     
     command = "python3"
     args = c("/nfs/users/nfs_j/jm33/apps/clinical-filter/src/main/python/clinical_filter.py",
-        "--ped",  new_path,
-        "--output", "test.txt",
-        "--syndrome-regions", "/lustre/scratch113/projects/ddd/resources/decipher_syndrome_list_20140428.txt",
-        "--known-genes", "/nfs/ddd0/Data/datafreeze/1133trios_20131218/DDG2P_with_genomic_coordinates_20131107_updated_TTN.tsv",
-        "--debug-chrom", chrom,
-        "--debug-pos", pos)
+    "--ped",  new_path,
+    "--output", "test.txt",
+    "--syndrome-regions", "/lustre/scratch113/projects/ddd/resources/decipher_syndrome_list_20140428.txt",
+    "--known-genes", "/lustre/scratch113/projects/ddd/resources/ddd_data_releases/2014-11-04/DDG2P/dd_genes_for_clinical_filter",
+    "--debug-chrom", chrom,
+    "--debug-pos", pos)
     
     reason = system2(command, args, stdout=TRUE)
     # allow for character(0) standard out
@@ -117,25 +117,25 @@ check_variant_fail <- function(variant, ped_path) {
 # aggregate the fail categories, so we can easily make tables
 combine_fail_categories <- function(variants) {
     
-    variants$fail_category = NA
+    category = rep(NA, nrow(variants)
     
     # I've manually set all the failure categories. Most of the variants fail for
     # the MAF, polyphen prediction, and VQSLOD categories.
-    variants$fail_category[grepl("VQSLOD", variants$fail_reason)] = "failed VQSLOD"
-    variants$fail_category[grepl("not smaller_than 0\\.01", variants$fail_reason)] = "failed MAF"
-    variants$fail_category[grepl("low MAF in non-biallelic", variants$fail_reason)] = "failed MAF"
-    variants$fail_category[grepl("variant not in VCF", variants$fail_reason)] = "variant not in VCF"
-    variants$fail_category[grepl("polyphen prediction", variants$fail_reason)] = "polyphen changed"
-    variants$fail_category[grepl("failed DENOVO-SNP\\/INDEL check", variants$fail_reason)] = "not called by denovogear"
-    variants$fail_category[grepl("gatk_LowQual", variants$fail_reason)] = "failed gatk filter"
-    variants$fail_category[grepl("gatk_StandardFilters", variants$fail_reason)] = "failed gatk filter"
-    variants$fail_category[grepl("TEAM29_FILTER", variants$fail_reason)] = "failed denovogear filters"
-    variants$fail_category[grepl("PP_DNM", variants$fail_reason)] = "failed PP_DNM"
-    variants$fail_category[grepl("failed CQ", variants$fail_reason)] = "consequence changed"
-    variants$fail_category[grepl("failed HGNC", variants$fail_reason)] = "gene symbol not picked up"
-     variants$fail_category[is.na(variants$fail_reason)] = "likely missing compound pair"
+    category[grepl("LOW_VQSLOD", variants$reason)] = "failed LOW_VQSLOD"
+    category[grepl("not smaller_than 0\\.01", variants$reason)] = "failed MAF"
+    category[grepl("low MAF in non-biallelic", variants$reason)] = "failed MAF"
+    category[grepl("variant not in VCF", variants$reason)] = "variant not in VCF"
+    category[grepl("polyphen prediction", variants$reason)] = "polyphen changed"
+    category[grepl("failed DENOVO-SNP\\/INDEL check", variants$reason)] = "not called by denovogear"
+    category[grepl("gatk_LowQual", variants$reason)] = "failed gatk filter"
+    category[grepl("gatk_StandardFilters", variants$reason)] = "failed gatk filter"
+    category[grepl("TEAM29_FILTER", variants$reason)] = "failed denovogear filters"
+    category[grepl("PP_DNM", variants$reason)] = "failed PP_DNM"
+    category[grepl("failed CQ", variants$reason)] = "consequence changed"
+    category[grepl("failed HGNC", variants$reason)] = "gene symbol not picked up"
+    category[is.na(variants$reason)] = "likely missing compound pair"
     
-    return(variants)
+    return(category)
 }
 
 main <- function() {
@@ -170,23 +170,23 @@ main <- function() {
     dev.off()
     
     # check the reasons why each variant failed
-    initial_only$fail_reason = apply(initial_only, 1, check_variant_fail, ORIGINAL_PED)
-    current_only$fail_reason = apply(current_only, 1, check_variant_fail, CURRENT_PED)
+    initial_only$reason = apply(initial_only, 1, check_variant_fail, CURRENT_PED)
+    current_only$reason = apply(current_only, 1, check_variant_fail, ORIGINAL_PED)
     
     # categorize the fail reasons
-    initial_only = combine_fail_categories(initial_only)
-    current_only = combine_fail_categories(current_only)
+    initial_only$category = combine_fail_categories(initial_only)
+    current_only$category = combine_fail_categories(current_only)
     
     # get the numbers of variants in each fail category (in the inital_only
     # cross-tabulate by reporting decision)
-    table(initial_only$fail_category, initial_only$DECISION)
-    table(current_only$fail_category)
+    table(initial_only$category, initial_only$DECISION)
+    table(current_only$category)
     
     # construct_pedigree_file("DDDP100149", "~/exome_reporting_2.ped", "temp.ped")
     # check_variant_fail(list(proband="DDDP100099", chrom=6, position=152712420), "~/samples.ped")
     
     # missing = read.table("~/problem_de_novos.txt", header=TRUE, sep="\t", stringsAsFactors=FALSE)
-    # missing$fail_reason = apply(missing, 1, check_variant_fail, "~/exome_reporting_2.ped")
+    # missing$reason = apply(missing, 1, check_variant_fail, "~/exome_reporting_2.ped")
     # write.table(missing, file="~/problem_de_novos.txt", row.names=FALSE, sep="\t", quote=FALSE)
     
 }
