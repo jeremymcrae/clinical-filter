e4e484ba409f8a9cf2d433298612a5aff3fe418d
Jeremy McRae <jm33@sanger.ac.uk>
2016-08-17 11:14:35 +0100

    added license text to files

--- tests/test_clinical_filter.py
+++ tests/test_clinical_filter.py
@@ -1,9 +1,27 @@
-""" unit testing of the ClinicalFilter class
-"""
+'''
+Copyright (c) 2016 Genome Research Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
 
 import unittest
 
-from clinical_filter import ClinicalFilter
+from clinicalfilter.filter import Filter
 from clinicalfilter.ped import Family
 from clinicalfilter.load_options import LoadOptions
 from clinicalfilter.variant.snv import SNV
@@ -32,7 +50,7 @@ class TestClinicalFilterPy(unittest.TestCase):
             pp_filter = 0.9
             lof_sites = None
         
-        self.finder = ClinicalFilter(opts)
+        self.finder = Filter(opts)
     
     def create_snv(self, sex, genotype, cq="missense_variant", hgnc="TEST", chrom="1"):
         """ create a default variant
