cb0e95be1cfdc380827a5534e885d8f561002b71
Jeremy McRae <jm33@sanger.ac.uk>
2016-10-04 19:54:20 +0100

    update version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name = "clinical-filter",
-    version = "0.4.7",
+    version = "0.4.8",
     author = "Jeremy McRae",
     author_email = "jeremy.mcrae@sanger.ac.uk",
     description="Clinical filtering for trios.",
