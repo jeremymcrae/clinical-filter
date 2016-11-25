c1f75e4cb8bc2ad933452942d6658c702e475b5c
Jeremy McRae <jm33@sanger.ac.uk>
2016-11-25 13:44:37 +0000

    fix long lines in argument parsing code

--- clinicalfilter/load_options.py
+++ clinicalfilter/load_options.py
@@ -30,32 +30,50 @@ def get_options():
     
     # the --ped and --child options are mutually exclusive
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--ped", help="Path to ped file containing cohort details for multiple trios.")
+    group.add_argument("--ped",
+        help="Path to ped file containing cohort details for multiple trios.")
     group.add_argument("--child", help="Path to child's VCF file.")
     
     parser.add_argument("--mother", help="Path to mother's VCF file.")
     parser.add_argument("--father", help="Path to father's VCF file.")
     parser.add_argument("--gender", help="The child's gender (male or female).")
-    parser.add_argument("--mom-aff", help="Mother's affected status (1=unaffected, or 2=affected).")
-    parser.add_argument("--dad-aff", help="Father's affected status (1=unaffected, or 2=affected).")
+    parser.add_argument("--mom-aff",
+        help="Mother's affected status (1=unaffected, or 2=affected).")
+    parser.add_argument("--dad-aff",
+        help="Father's affected status (1=unaffected, or 2=affected).")
     
-    parser.add_argument("--syndrome-regions", dest="regions", help="Path to list of CNV regions known to occur in disorders.")
-    parser.add_argument("--known-genes", help="Path to table of known disease causative genes.")
-    parser.add_argument("--known-genes-date", dest="genes_date", help="Date that the list of known disease causative genes was last updated, used to track the version of known-genes used for analysis.")
-    parser.add_argument("-o", "--output", help="Path for analysis output in tabular format.")
-    parser.add_argument("--export-vcf", help="Directory or file path for analysis output in VCF format.")
-    parser.add_argument("--log", dest="loglevel", default="debug", help="Level of logging to use, choose from: debug, info, warning, error or critical.")
-    parser.add_argument("--debug-chrom", help="chromosome of variant for which to debug the filtering behaviour.")
-    parser.add_argument("--debug-pos", type=int, help="position of variant for which to debug the filtering behaviour.")
-    parser.add_argument("--lof-sites", help="path to file of sites at the last base of exons that are potentially LoF sites.")
+    parser.add_argument("--syndrome-regions", dest="regions",
+        help="Path to list of CNV regions known to occur in disorders.")
+    parser.add_argument("--known-genes",
+        help="Path to table of known disease causative genes.")
+    parser.add_argument("--known-genes-date", dest="genes_date",
+        help="Date that the list of known disease causative genes was last"
+            "updated, used to track the version of known-genes used for"
+            "analysis.")
+    parser.add_argument("-o", "--output",
+        help="Path for analysis output in tabular format.")
+    parser.add_argument("--export-vcf",
+        help="Directory or file path for analysis output in VCF format.")
+    parser.add_argument("--log", dest="loglevel", default="debug",
+        help="Level of logging to use, choose from: debug, info, warning,"
+            "error or critical.")
+    parser.add_argument("--debug-chrom",
+        help="chromosome of variant for which to debug the filtering behaviour.")
+    parser.add_argument("--debug-pos", type=int,
+        help="position of variant for which to debug the filtering behaviour.")
+    parser.add_argument("--lof-sites",
+        help="path to file of sites at the last base of exons that are "
+            "potentially LoF sites.")
     
     # New argument added by PJ to allow DNM_PP filtering to be disabled.
     parser.add_argument("--pp-dnm-threshold", dest="pp_filter", type=float,
-        default=0.0, help="Set PP_DNM threshold for filtering (defaults to >=0.9)")
+        default=0.0,
+        help="Set PP_DNM threshold for filtering (defaults to >=0.9)")
     parser.add_argument("--maf-populations",
-        default="AFR_AF,AMR_AF,ASN_AF,DDD_AF,EAS_AF,ESP_AF,EUR_AF,MAX_AF,SAS_AF,UK10K_cohort_AF",
-        help="Comma separated list of population tags that can exist in the INFO"
-            "field for population-specific minor allele frequencies")
+        default="AFR_AF,AMR_AF,ASN_AF,DDD_AF,EAS_AF,ESP_AF,EUR_AF,MAX_AF,"
+            "SAS_AF,UK10K_cohort_AF",
+        help="Comma separated list of population tags that can exist in the "
+            "INFO field for population-specific minor allele frequencies")
     
     args = parser.parse_args()
     
